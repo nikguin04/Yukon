667a3864eff30a1d187ae733ec7b89f2c8f7bde8
Niklas Jensen <niklaspj2004@gmail.com>
2024-04-14 13:01:53 +0200

    Command error handling while playing / not playing

--- src/controller/command_controller.c
+++ src/controller/command_controller.c
@@ -5,41 +5,63 @@
 #include <stdlib.h>
 
 const char *LoadDeckFromFile(Controller *ctrl, char *path) {
-	const char *msg;
-	ctrl->model->deck = LoadDeck(path, &msg);
-	DeckToYukon(ctrl->model->deck, ctrl->model->yukon, COLUMN_LOADSIZE);
-	return msg;
+    if (!ctrl->model->yukon->play_phase) {
+        const char *msg;
+        ctrl->model->deck = LoadDeck(path, &msg);
+        DeckToYukon(ctrl->model->deck, ctrl->model->yukon, COLUMN_LOADSIZE);
+        return msg;
+    } else {
+        return "Cannot load a deck while playing game";
+    }
 }
 
 const char *SaveDeckToFile(Controller *ctrl, char *path) {
-	const char *msg;
-	SaveDeck(ctrl->model->deck, path, &msg);
-	return msg;
+
+    if (!ctrl->model->yukon->play_phase) {
+        	const char *msg;
+        SaveDeck(ctrl->model->deck, path, &msg);
+        return msg;
+    } else {
+        return "Cannot save a deck while playing game";
+    }
 }
 
 const char *ShowDeck(Controller *ctrl, char *_) {
-	printf("Showing deck to user!\n");
-	ctrl->model->optionIgnoreHidden = true;
-	return "Showed deck to user!";
+    if (!ctrl->model->yukon->play_phase) {
+        printf("Showing deck to user!\n");
+        ctrl->model->optionIgnoreHidden = true;
+        return "Showed deck to user!";
+    } else {
+        return "Cannot show deck while playing";
+    }
 }
 
 const char *ShuffleInterleaving(Controller *ctrl, char *split) {
-	const char *msg;
-	if (split == NULL) {
-		ctrl->model->deck = shuffleInterleaving(ctrl->model->deck, 2, &msg, true);
-		DeckToYukon(ctrl->model->deck, ctrl->model->yukon, COLUMN_LOADSIZE);
-		return msg;
-	} else {
-		ctrl->model->deck = shuffleInterleaving(ctrl->model->deck, atoi(split), &msg, false);
-		DeckToYukon(ctrl->model->deck, ctrl->model->yukon, COLUMN_LOADSIZE);
-		return msg;
-	}
+    if (!ctrl->model->yukon->play_phase) {
+        const char *msg;
+        if (split == NULL) {
+            ctrl->model->deck = shuffleInterleaving(ctrl->model->deck, 2, &msg, true);
+            DeckToYukon(ctrl->model->deck, ctrl->model->yukon, COLUMN_LOADSIZE);
+            return msg;
+        } else {
+            ctrl->model->deck = shuffleInterleaving(ctrl->model->deck, atoi(split), &msg, false);
+            DeckToYukon(ctrl->model->deck, ctrl->model->yukon, COLUMN_LOADSIZE);
+            return msg;
+        }
+    } else {
+        return "Cannot shuffle deck interleaving while playing";
+    }
+
 }
 
 const char *ShuffleRandom(Controller *ctrl, char *split) {
-	ctrl->model->deck = shuffleRandom(ctrl->model->deck);
-	DeckToYukon(ctrl->model->deck, ctrl->model->yukon, COLUMN_LOADSIZE);
-	return "Deck randomly shuffled";
+    if (!ctrl->model->yukon->play_phase) {
+        ctrl->model->deck = shuffleRandom(ctrl->model->deck);
+        DeckToYukon(ctrl->model->deck, ctrl->model->yukon, COLUMN_LOADSIZE);
+        return "Deck randomly shuffled";
+    } else {
+        return "Cannot shuffle deck at random while playing";
+    }
 }
 
 const char *QuitAndExit(Controller *_c, char *_i) {
